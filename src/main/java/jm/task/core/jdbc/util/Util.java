94ef42c83d803f329d0215bd2a255597c238aa4b
casinocool <casinocool@inbox.ru>
2025-11-02 02:21:14 +0300

    hiberADD

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.cfg.Environment;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Properties;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+    private static SessionFactory sessionFactory;
+    private static EntityManagerFactory entityManagerFactory;
     public static Connection connectDatabase() {
         Connection connection = null;
         try {
@@ -24,5 +35,34 @@ public class Util {
                 e.printStackTrace();
             }
         }
+    }
+    public static SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration();
+        if (sessionFactory == null) {
+            try {
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "org.postgresql.Driver");
+                settings.put(Environment.URL, "jdbc:postgresql://localhost:5432/test");
+                settings.put(Environment.USER, "postgres");
+                settings.put(Environment.PASS, "12345");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.FORMAT_SQL, "true");
+                settings.put(Environment.HBM2DDL_AUTO, "none");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.DEFAULT_SCHEMA, "public");
+                settings.put(Environment.JDBC_TIME_ZONE, "UTC");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                sessionFactory = configuration.buildSessionFactory();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
